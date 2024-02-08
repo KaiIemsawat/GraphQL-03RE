01b2a74acf791020f3357d7114e751ae057296e1
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-02-07 20:07:09 -0500

    update schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,6 +5,7 @@ const {
     GraphQLID,
     GraphQLString,
     GraphQLSchema,
+    GraphQLList,
 } = require("graphql");
 
 // Client Type
@@ -18,14 +19,44 @@ const ClientType = new GraphQLObjectType({
     }),
 });
 
+// Client Type
+const ProjectType = new GraphQLObjectType({
+    name: "Project",
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString },
+    }),
+});
+
 const RootQuery = new GraphQLObjectType({
     name: "RootQueryType",
     fields: {
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parent, args) {
+                return clients;
+            },
+        },
         client: {
             type: ClientType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 return clients.find((client) => client.id === args.id);
+            },
+        },
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return projects;
+            },
+        },
+        project: {
+            type: ProjectType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return projects.find((project) => project.id === args.id);
             },
         },
     },
@@ -34,3 +65,19 @@ const RootQuery = new GraphQLObjectType({
 module.exports = new GraphQLSchema({
     query: RootQuery,
 });
+
+/* 
+Later, we may go to localhost:<number>/graphql
+and try 
+{
+  client(id: "1") {
+    id, name, email, phone
+  }
+}
+or
+{
+  clients {
+    id, name, email, phone
+  }
+}
+*/
