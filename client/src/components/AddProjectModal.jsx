03987e9a322c77ae459c8ca08e85ff15d96728ef
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-02-14 14:11:41 -0500

    Add new project functioning

--- client/src/components/AddProjectModal.jsx
+++ client/src/components/AddProjectModal.jsx
@@ -3,6 +3,7 @@ import { useMutation, useQuery } from "@apollo/client";
 import { FaList } from "react-icons/fa";
 import { GET_CLIENTS } from "../queries/clientQueries";
 import { GET_PROJECTS } from "../queries/projectQueries";
+import { ADD_PROJECT } from "../mutations/projectMutation";
 
 const AddProjectModal = () => {
     const [name, setName] = useState("");
@@ -10,14 +11,27 @@ const AddProjectModal = () => {
     const [clientId, setClientId] = useState("");
     const [status, setStatus] = useState("new");
 
+    const [addProject] = useMutation(ADD_PROJECT, {
+        variables: { name, description, clientId, status },
+        update(cache, { data: { addProject } }) {
+            const { projects } = cache.readQuery({ query: GET_PROJECTS });
+            cache.writeQuery({
+                query: GET_PROJECTS,
+                data: { projects: [...projects, addProject] },
+            });
+        },
+    });
+
     const { loading, error, data } = useQuery(GET_CLIENTS);
 
     const onSubmit = (e) => {
         e.preventDefault();
 
         if (name === "" || description === "" || status === "") {
-            return alert("All fields are required");
+            return alert("Please fill in all fields");
         }
+
+        addProject(name, description, clientId, status);
 
         setName("");
         setDescription("");
